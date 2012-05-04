767f45318afd9e3c5c33591ce46b9c3836a5d88f
MacAir <macroot@MacBook-Air.local>
2012-05-04 22:02:22 +0300

    Proper require directives

--- src/who_is_twitter/core.clj
+++ src/who_is_twitter/core.clj
@@ -1,16 +1,9 @@
-(ns who-is-twitter.core
-	(:use
-		[twitter.oauth]
-		[twitter.callbacks]
-		[twitter.callbacks.handlers]
-		[twitter.api.restful]
-	)
-	(:import
-		(twitter.callbacks.protocols SyncSingleCallback)
-	)
-)
+(ns who-is-twitter.core)
 
-(def ^:dynamic *creds* (make-oauth-creds 
+(require '(twitter [oauth :as oauth]))
+(require '(twitter.api [restful :as twitter]))
+
+(def ^:dynamic *creds* (oauth/make-oauth-creds 
 						"fKxjEt5kGvr1UOPJEHsuVQ"
 						"joQHXFhCShNIllhmfd0RIg7GZ8mWagf6VnDnXL1cqw"
 						"223136824-JfB6BQjqejeIeNRI3i6uHoCHFkKy8bz1dH3MR0qd"
@@ -19,18 +12,16 @@
 
 (defn -main [& args]
 	(println "Starting whois for twitter")
-	; (let [user-info 
-	; 		((show-user 
-	; 			:oauth-creds *creds* 
-	; 			:params {:screen-name "AdamJWynne"}
-	; 		) :body)
-	; 	]
-	; )
-	(def user-info {:name "Volodymyr"})
+	(let [user-info 
+			((twitter/show-user 
+				:oauth-creds *creds* 
+				:params {:screen-name "AdamJWynne"}
+			) :body)
+			]
 		(println "Full name:        " (user-info :name))
 		(println "Screen name:      " (user-info :screen_name))
 		(println "Self description: " (user-info :description))
 		(println "Location :        " (user-info :location))
-		(println "Web :             " (user-info :url))
-	; )
+		(println "Web :             " (user-info :url)))
+	(System/exit 0)
 )
